590e4b273c5b6e397de95e417e00e81d79198851
Xuskia11 <atuka.09.123@gmail.com>
2024-06-18 07:41:03 -0700

    commit

---
+++ Day 41/homework/script.js
@@ -0,0 +1,7 @@
+let num = Number(prompt("Enter random number here: "));
+let num2 = Number(prompt("Enter second random number here: "));
+
+console.log(num + num2);
+console.log(num - num2);
+console.log(num * num2);
+console.log(num / num2);
