cb90a4d9c16d3e22516fd4526f573281d5077835
Xuskia11 <atuka.09.123@gmail.com>
2024-07-21 16:24:42 +0400

    commit

---
+++ Day 55/classwork/script.js
@@ -0,0 +1,36 @@
+let number = 5
+let number2 = 10
+
+if(number < number2 && number > 9){
+    console.log("number2 is leading")
+} else if(number > number2){
+    console.log("number1 is laeding")
+} else if (number === number2){
+    console.log("Those numbers are equeal")
+} else {
+    console.log("Those numbers are useless")
+};
+
+
+for(let i = 0;i < 5; i++){
+    console.log(i);
+};
+
+
+
+const lists = [1,2,3,4,5,6,7];
+
+for(const i of lists){
+    if(i % 2 === 0){
+        console.log(i + " " + "This number is even");
+
+    } else {
+        console.log(i + " " + "This number is odd");
+    }
+};
+
+let number5 = 200;
+while(number5 !== 0){
+    number5--;
+    console.log(number5);
+}
